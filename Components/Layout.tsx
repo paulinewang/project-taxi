0c1d7938ed017096a71d71039244d2cfc3bbc33e
Pauline Wang <wbl.pauline@gmail.com>
2022-02-25 18:54:28 +0100

    show all players; toggle ui depending on auth status

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -1,11 +1,20 @@
-import { ReactNode } from "react";
+import { ReactNode, useState, useEffect } from "react";
 import styled from "styled-components";
+import firebase from "firebase/compat/app";
+
 import Header from "./Header";
 import InviteButton from "./InviteButton";
 import OngoingGame from "./OngoingGame";
+import PlayerList from "./PlayerList";
 
 const Main = styled.main`
-  flex: 1;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const RightColumn = styled.div`
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 type LayoutProps = {
@@ -13,13 +22,47 @@ type LayoutProps = {
 };
 
 const Layout = ({ children }: LayoutProps) => {
+  const [players, setPlayers] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [currentUserId, setCurrentUserId] = useState<string | undefined>("");
+
+  useEffect(() => {
+    firebase
+      .auth()
+      .onAuthStateChanged((user: any) => setCurrentUserId(user?.uid));
+
+    const id = firebase.auth().currentUser?.uid;
+    setCurrentUserId(id);
+  }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("api/hello")
+      .then((res) => res.json())
+      .then((data) => {
+        setPlayers(data.userRecords.users);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <Header />
       <Main>
-        <InviteButton />
-        <OngoingGame />
-        {children}
+        {currentUserId ? (
+          <>
+            <PlayerList
+              players={!isLoading ? players : []}
+              currentUserId={currentUserId}
+            />
+            <RightColumn>
+              <InviteButton />
+              <OngoingGame />
+            </RightColumn>
+          </>
+        ) : (
+          <p>Please sign in</p>
+        )}
       </Main>
     </>
   );
