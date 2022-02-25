d06228eeafb0fe2a334d185c838081d2a5b46e22
Maxi Cespedes <maxi.cespedes@wetransfer.com>
2022-02-25 09:52:29 +0100

    firebase initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { initializeApp } from 'firebase/app';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyARl7ImqyHzciv2xZe_PNlUXs0ACdxeHbY",
+  authDomain: "project-taxi-8a2bf.firebaseapp.com",
+  databaseURL: "https://project-taxi-8a2bf-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "project-taxi-8a2bf",
+  storageBucket: "project-taxi-8a2bf.appspot.com",
+  messagingSenderId: "529990881670",
+  appId: "1:529990881670:web:c3b73b56fbe6e27b849c07"
+};
+
+const app = initializeApp(firebaseConfig);
+console.log(app);
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
