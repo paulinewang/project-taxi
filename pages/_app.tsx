743135972f954835c37a45d4c54fea4b1a61c59f
Maxi Cespedes <maxi.cespedes@wetransfer.com>
2022-02-25 11:23:24 +0100

    add styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { initializeApp } from 'firebase/app';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { initializeApp } from "firebase/app";
+import { ThemeProvider } from "styled-components";
+import theme from "../shared/theme";
+import GlobalStyle from "../shared/GlobalStyle";
 
 const firebaseConfig = {
   apiKey: "AIzaSyARl7ImqyHzciv2xZe_PNlUXs0ACdxeHbY",
   authDomain: "project-taxi-8a2bf.firebaseapp.com",
-  databaseURL: "https://project-taxi-8a2bf-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://project-taxi-8a2bf-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "project-taxi-8a2bf",
   storageBucket: "project-taxi-8a2bf.appspot.com",
   messagingSenderId: "529990881670",
-  appId: "1:529990881670:web:c3b73b56fbe6e27b849c07"
+  appId: "1:529990881670:web:c3b73b56fbe6e27b849c07",
 };
+
+
+
 
 const app = initializeApp(firebaseConfig);
 console.log(app);
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
