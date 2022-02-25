8a9c8061cb2289be2e68246813b3602163ea6a36
Nickie Roudez <nickie.roudez@gmail.com>
2022-02-25 10:58:15 +0100

    Create authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyARl7ImqyHzciv2xZe_PNlUXs0ACdxeHbY",
@@ -12,10 +13,17 @@ const firebaseConfig = {
   appId: "1:529990881670:web:c3b73b56fbe6e27b849c07"
 };
 
-const app = initializeApp(firebaseConfig);
-console.log(app);
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+console.log(auth);
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component firebase={firebase} {...pageProps} />
 }
 
 export default MyApp
