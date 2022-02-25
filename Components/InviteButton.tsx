1dca7452adcf4956568f287247470b63d463d7fa
Maxi Cespedes <maxi.cespedes@wetransfer.com>
2022-02-25 16:30:43 +0100

    connect to database and push one example alert

--- Components/InviteButton.tsx
+++ Components/InviteButton.tsx
@@ -1,20 +1,70 @@
 import styled from "styled-components";
+import { getDatabase, ref, set, onValue } from "firebase/database";
+import { userInfo } from "os";
+import { useEffect, useState } from "react";
 
-const Button = styled.button`
+const Button = styled.button<{ disabledCustom: boolean }>`
   text-transform: uppercase;
   font: Roboto;
   font-weight: bold;
   font-size: 32px;
   padding: 10px 40px;
   background-color: white;
-  cursor: pointer;
-  border: 3px solid ${({ theme }) => theme.colors.yellow};
-  box-shadow: -0.5rem 0.5rem ${({ theme }) => theme.colors.yellow};
+  color: ${({ disabledCustom }) =>
+  disabledCustom ? "#CCC" : "black"};
+  cursor: ${({ disabledCustom }) =>
+    disabledCustom ? "not-allowed" : "pointer"};
+  border: 3px solid
+    ${({ theme, disabledCustom }) =>
+      disabledCustom ? "#CCC " : theme.colors.yellow};
+  box-shadow: -0.5rem 0.5rem
+    ${({ theme, disabledCustom }) =>
+      disabledCustom ? "#CCC" : theme.colors.yellow};
 `;
 
 const InviteButton = () => {
+  const [alerts, setAlerts] = useState({});
+
+  useEffect(() => {
+    const getAlerts = () => {
+      const db = getDatabase();
+      const alertsRef = ref(db, "alerts/1");
+      onValue(alertsRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+        setAlerts(data);
+      });
+    };
+
+    getAlerts();
+  }, [setAlerts]);
+
+  const gameInProgress = true;
+  const isOwner = false;
+
+  const disabled = gameInProgress && isOwner;
+
+  const createInvite = async () => {
+    if (disabled) {
+      return;
+    }
+
+    const db = getDatabase();
+
+    const res = await set(ref(db, "alerts/1"), {
+      owner: "neomaxzero@gmail.com",
+      participants: [
+        { email: "pauline.wang@wetransfer.com", state: "ACCEPTED" },
+      ],
+    });
+
+    console.log("Succeed?", res);
+  };
+
   return (
-    <Button onClick={() => console.log("sending invite")}>Send request</Button>
+    <Button disabledCustom={disabled} onClick={createInvite}>
+      Send request
+    </Button>
   );
 };
 
